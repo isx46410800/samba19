44090fbb415042cbffe72a40ce5db96b93a22900
Miguel Amorós <miguel14amoros@gmail.com>
2019-12-02 08:17:21 +0100

    samba base con usuaris

--- samba19:base/startup.sh
+++ samba19:base/startup.sh
@@ -2,9 +2,23 @@
 # @edt ASIX M06 2019-2020
 # startup.sh
 # -------------------------------------
-
+#instalacio / preparacio
 /opt/docker/install.sh && echo "Install Ok"
 /usr/sbin/smbd && echo "smb Ok"
 /usr/sbin/nmbd && echo "nmb  Ok"
-/bin/bash
+
+# creacio usuaris
+useradd lial
+useradd roc
+useradd patipla
+useradd pla
+echo -e "lila\nlila" | smbpasswd -a lila
+echo -e "roc\nroc" | smbpasswd -a roc
+echo -e "patipla\npatipla" | smbpasswd -a patipla
+echo -e "pla\npla" | smbpasswd -a pla
+
+# activar els serveis
+/usr/sbin/smbd -d && echo "smb Ok"
+/usr/sbin/nmbd -d && echo "nmb  Ok"
+/bin/bash 
 
